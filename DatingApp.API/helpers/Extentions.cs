3c361a758072270eb9fe3d23d96c6c9dccd60eaa
swarup1510laddha <swarup15101996@gmail.com>
2020-06-05 20:06:04 +0530

    end of section 8

--- DatingApp.API/helpers/Extentions.cs
+++ DatingApp.API/helpers/Extentions.cs
@@ -1,14 +1,24 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.helpers
 {
     public static class Extentions
     {
-        public static void AddApplicationError(this HttpResponse response,string message)
+        public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
-            response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
-            response.Headers.Add("Access-Control-Allow-Origin","*");
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+
+            return age;
         }
     }
 }
